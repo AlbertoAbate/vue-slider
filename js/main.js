1e3663c672c448d58804f691e1fb02bc97caadd9
Alberto <alberto.abate87@gmail.com>
2020-11-09 17:44:35 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@ const app = new Vue({
       "./img/image2.jpg",
       "./img/image3.jpg",
       "./img/image4.jpg"
-    ]
+    ],
+    intervalId: 0
+  },
+  created() {
+    this.startLoop();
   },
   methods: {
     nextPhoto() {
@@ -28,6 +32,14 @@ const app = new Vue({
     },
     setPhoto(index) {
       this.indexPhoto = index;
+    },
+    startLoop() {
+      this.intervalId = setInterval(() => {
+        this.nextPhoto();
+      }, 3000);
+    },
+    stopLoop() {
+      clearInterval(this.intervalId)
     }
   }
 });
